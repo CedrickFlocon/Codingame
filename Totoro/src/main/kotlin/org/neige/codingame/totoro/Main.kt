23c8f1c04cc2b251fbe5e2f53643b7524208ff6d
Cedrick Flocon <7161169+CedrickFlocon@users.noreply.github.com>
2021-05-06 16:29:07 +0200

    Totoro project setup

---
+++ Totoro/src/main/kotlin/org/neige/codingame/totoro/Main.kt
@@ -0,0 +1,50 @@
+package org.neige.codingame.totoro
+
+import java.util.Scanner
+
+fun main() {
+    val input = Scanner(System.`in`)
+    val numberOfCells = input.nextInt() // 37
+    for (i in 0 until numberOfCells) {
+        val index = input.nextInt() // 0 is the center cell, the next cells spiral outwards
+        val richness = input.nextInt() // 0 if the cell is unusable, 1-3 for usable cells
+        val neigh0 = input.nextInt() // the index of the neighbouring cell for each direction
+        val neigh1 = input.nextInt()
+        val neigh2 = input.nextInt()
+        val neigh3 = input.nextInt()
+        val neigh4 = input.nextInt()
+        val neigh5 = input.nextInt()
+    }
+
+    // game loop
+    while (true) {
+        val day = input.nextInt() // the game lasts 24 days: 0-23
+        val nutrients = input.nextInt() // the base score you gain from the next COMPLETE action
+        val sun = input.nextInt() // your sun points
+        val score = input.nextInt() // your current score
+        val oppSun = input.nextInt() // opponent's sun points
+        val oppScore = input.nextInt() // opponent's score
+        val oppIsWaiting = input.nextInt() != 0 // whether your opponent is asleep until the next day
+        val numberOfTrees = input.nextInt() // the current amount of trees
+        for (i in 0 until numberOfTrees) {
+            val cellIndex = input.nextInt() // location of this tree
+            val size = input.nextInt() // size of this tree: 0-3
+            val isMine = input.nextInt() != 0 // 1 if this is your tree
+            val isDormant = input.nextInt() != 0 // 1 if this tree is dormant
+        }
+        val numberOfPossibleMoves = input.nextInt()
+        if (input.hasNextLine()) {
+            input.nextLine()
+        }
+        for (i in 0 until numberOfPossibleMoves) {
+            val possibleMove = input.nextLine()
+        }
+
+        // Write an action using println()
+        // To debug: System.err.println("Debug messages...");
+
+
+        // GROW cellIdx | SEED sourceIdx targetIdx | COMPLETE cellIdx | WAIT <message>
+        println("WAIT")
+    }
+}
